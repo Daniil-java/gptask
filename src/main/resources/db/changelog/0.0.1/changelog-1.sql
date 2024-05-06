45ade5e8b45f9da1de9db6a26cf763e06f490fb1
ПК <saponembigas@gmail.com>
2024-05-06 20:51:13 +0700

    feat(et-1): Настройка liquibase & docker-compose.yml

---
+++ src/main/resources/db/changelog/0.0.1/changelog-1.sql
@@ -0,0 +1,25 @@
+--liquibase formatted sql
+
+--changeset DanielK:1
+CREATE TABLE IF NOT EXISTS users
+(
+    id              serial primary key,
+    name            text,
+    created         timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id              serial not null primary key,
+    user_id         int not null references users(id),
+    parent_id       int not null references tasks(id),
+    name            text,
+    priority        smallint not null DEFAULT 0,
+    status          smallint not null DEFAULT 0,
+    comment         text,
+    updated         timestamp DEFAULT current_timestamp,
+    created         timestamp DEFAULT current_timestamp
+);
+
+CREATE INDEX parent_id ON tasks(parent_id);
+
