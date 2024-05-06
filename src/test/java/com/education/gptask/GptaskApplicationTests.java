37123b33882bdb739f3d740492859a53cc2ab360
ПК <saponembigas@gmail.com>
2024-05-06 18:42:30 +0700

    initialization

---
+++ src/test/java/com/education/gptask/GptaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.gptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
