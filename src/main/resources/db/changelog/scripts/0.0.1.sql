698170179bb28cd838d2f9ed3b178eec8afd34c0
ПК <saponembigas@gmail.com>
2024-05-07 20:19:26 +0700

    feat(et-1): review fix

--- src/main/resources/db/changelog/scripts/0.0.1.sql
+++ src/main/resources/db/changelog/scripts/0.0.1.sql
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS tasks
 (
     id              serial not null primary key,
     user_id         int not null references users(id),
-    parent_id       int not null references tasks(id),
+    parent_id       int references tasks(id),
     name            text,
     priority        smallint not null DEFAULT 0,
     status          smallint not null DEFAULT 0,
     comment         text,
-    updated         timestamp DEFAULT current_timestamp,
+    updated         timestamp,
     created         timestamp DEFAULT current_timestamp
-);
+    );
 
 CREATE INDEX parent_id ON tasks(parent_id);
 
