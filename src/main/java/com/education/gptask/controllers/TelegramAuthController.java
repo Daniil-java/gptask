991acd0a451291106ed9f8b919098958f3304de8
ПК <saponembigas@gmail.com>
2024-09-26 21:16:47 +0700

    Telegram Auth fix

--- src/main/java/com/education/gptask/controllers/TelegramAuthController.java
+++ src/main/java/com/education/gptask/controllers/TelegramAuthController.java
@@ -2,6 +2,7 @@ package com.education.gptask.controllers;
 
 
 import com.education.gptask.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +18,12 @@ public class TelegramAuthController {
     private final UserService userService;
 
     @PostMapping("/telegram")
-    public String handleTelegramAuth(@RequestParam Map<String, String> queryParams) {
+    public String handleTelegramAuth(@RequestParam Map<String, String> queryParams, HttpServletRequest request) {
         if (userService.handleTelegramAuth(queryParams)) {
+            request.getSession(true);
             return "redirect:/";
         } else {
             return "login";
         }
     }
-
-
 }
